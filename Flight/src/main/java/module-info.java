adb8d41d6f6715dfbaf3fa548caf0d80dc10de6a
Hao <52395276+Chauchihao@users.noreply.github.com>
2021-04-07 22:45:42 +0700

    Update Database & Project

---
+++ Flight/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.mycompany.flight {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+    opens com.mycompany.flight to javafx.fxml;
+    exports com.mycompany.flight;
+}
