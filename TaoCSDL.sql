027f57a2ef9e6a5fceac5f93975d99fbeaf40e57
Hao <52395276+Chauchihao@users.noreply.github.com>
2021-04-13 10:10:42 +0700

    TaoCSDL Phiên Bản tạm thời hết lỗi

--- TaoCSDL.sql
+++ TaoCSDL.sql
@@ -1,101 +1,100 @@
-CREATE DATABASE `flight` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE flight /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE flight;
 
-CREATE TABLE `maybay` (
-  `soHieu` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `hangBay` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `soGheHang1` int NOT NULL,
-  `soGheHang2` int NOT NULL,
-  PRIMARY KEY (`soHieu`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE maybay (
+  soHieuMayBay varchar(10) NOT NULL,
+  hangBay varchar(100) NOT NULL,
+  soGheHang1 int NOT NULL,
+  soGheHang2 int NOT NULL,
+  PRIMARY KEY (soHieuMayBay)
+) ENGINE=InnoDB;
 
-CREATE TABLE `sanbay` (
-  `maSanBay` int NOT NULL,
-  `tenSanBay` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `quocGia` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `trangThai` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`maSanBay`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE sanbay (
+  maSanBay int NOT NULL,
+  tenSanBay varchar(255) NOT NULL,
+  quocGia varchar(255) NOT NULL,
+  trangThai varchar(255) NOT NULL,
+  PRIMARY KEY (maSanBay)
+) ENGINE=InnoDB;
 
-CREATE TABLE `user` (
-  `id` int NOT NULL,
-  `hoTen` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tenTK` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `matKhau` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `trangThai` tinyint(1) NOT NULL,
-  `loaiTK` enum('Admin','NV') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `diaChi` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `sdt` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE users (
+  id int NOT NULL,
+  hoTen varchar(255) NOT NULL,
+  tenTK varchar(255) NOT NULL,
+  matKhau varchar(255) NOT NULL,
+  trangThai bool NOT NULL,
+  loaiTK enum('Admin','NV') NOT NULL,
+  diaChi varchar(255) DEFAULT NULL,
+  sdt varchar(10) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
 
-CREATE TABLE `ghe` (
-  `maGhe` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `trangThai` tinyint(1) NOT NULL,
-  `hangGhe` int NOT NULL,
-  `soHieuMayBay` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`maGhe`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE ghe (
+  maGhe varchar(5) NOT NULL,
+  trangThai bool NOT NULL,
+  hangGhe int NOT NULL,
+  soHieuMayBay varchar(10) NOT NULL,
+  PRIMARY KEY (maGhe)
+) ENGINE=InnoDB;
 
+CREATE TABLE khachhang (
+  maKH int NOT NULL,
+  tenKH varchar(255) NOT NULL,
+  email varchar(100) NULL,
+  sdt varchar(10) NULL,
+  PRIMARY KEY (maKH)
+) ENGINE=InnoDB;
 
+CREATE TABLE chuyenbay (
+  maChuyenBay int NOT NULL,
+  ngayGioiKhoiHanh datetime NOT NULL,
+  thoiGianBay datetime NOT NULL,
+  thoiGianDung datetime NOT NULL,
+  maNoiDi int NOT NULL,
+  maNoiDen int NOT NULL,
+  soHieuMayBay varchar(10) NOT NULL,
+  sanBayTrungGian int DEFAULT NULL,
+  PRIMARY KEY (maChuyenBay)
+) ENGINE=InnoDB;
 
-CREATE TABLE `khachhang` (
-  `maKH` int NOT NULL,
-  `tenKH` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `sdt` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`maKH`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE `chuyenbay` (
-  `maChuyenBay` int NOT NULL,
-  `ngayGioiKhoiHanh` datetime NOT NULL,
-  `thoiGianBay` datetime NOT NULL,
-  `thoiGianDung` datetime NOT NULL,
-  `maNoiDi` int NOT NULL,
-  `maNoiDen` int NOT NULL,
-  `soHieuMayBay` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `sanBayTrungGian` int DEFAULT NULL,
-  PRIMARY KEY (`maChuyenBay`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE `phieudatcho` (
-  `maPhieu` int NOT NULL,
-  `maVe` int NOT NULL,
-  `maKH` int NOT NULL,
-  `ngayDatVe` datetime NOT NULL,
-  PRIMARY KEY (`maPhieu`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE phieudatcho (
+  maPhieu int NOT NULL,
+  maVe int NOT NULL,
+  maKH int NOT NULL,
+  ngayDatVe datetime NOT NULL,
+  PRIMARY KEY (maPhieu)
+) ENGINE=InnoDB;
 
 
 
 
 
-CREATE TABLE `vemaybay` (
-  `maVe` int NOT NULL,
-  `hangVe` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `giaVe` float NOT NULL,
-  `ngayXuatVe` datetime NOT NULL,
-  `maNV` int NOT NULL,
-  `maKH` int NOT NULL,
-  `maChuyenBay` int NOT NULL,
-  PRIMARY KEY (`maVe`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE vemaybay (
+  maVe int NOT NULL,
+  hangVe varchar(10) NOT NULL,
+  giaVe float NOT NULL,
+  ngayXuatVe datetime NOT NULL,
+  maNV int NOT NULL,
+  maKH int NOT NULL,
+  maChuyenBay int NOT NULL,
+  PRIMARY KEY (maVe)
+) ENGINE=InnoDB;
 
 
 
-ALTER TABLE `vemaybay` ADD CONSTRAINT `maChuyenBay` FOREIGN KEY (`maChuyenBay`) REFERENCES `chuyenbay` (`maChuyenBay`);
-ALTER TABLE `vemaybay` ADD CONSTRAINT `maKH` FOREIGN KEY (`maKH`) REFERENCES `khachhang` (`maKH`);
-ALTER TABLE `vemaybay` ADD CONSTRAINT `maNV` FOREIGN KEY (`maNV`) REFERENCES `user` (`id`);
+ALTER TABLE vemaybay ADD FOREIGN KEY (maChuyenBay) REFERENCES chuyenbay (maChuyenBay);
+ALTER TABLE vemaybay ADD FOREIGN KEY (maKH) REFERENCES khachhang (maKH);
+ALTER TABLE vemaybay ADD FOREIGN KEY (maNV) REFERENCES users (id);
 
-ALTER TABLE `chuyenbay` ADD CONSTRAINT `maNoiDi` FOREIGN KEY (`maNoiDi`) REFERENCES `sanbay` (`maSanBay`);
-ALTER TABLE `chuyenbay` ADD CONSTRAINT `maNoiDen` FOREIGN KEY (`maNoiDen`) REFERENCES `sanbay` (`maSanBay`);
-ALTER TABLE `chuyenbay` ADD CONSTRAINT `soHieuMayBay` FOREIGN KEY (`soHieuMayBay`) REFERENCES `maybay` (`soHieu`);
+ALTER TABLE chuyenbay ADD FOREIGN KEY (maNoiDi) REFERENCES sanbay (maSanBay);
+ALTER TABLE chuyenbay ADD FOREIGN KEY (maNoiDen) REFERENCES sanbay (maSanBay);
+ALTER TABLE chuyenbay ADD  FOREIGN KEY (soHieuMayBay) REFERENCES maybay (soHieuMayBay);
 
-#ALTER TABLE `ghe` ADD CONSTRAINT `soHieuMayBay` FOREIGN KEY (`soHieuMayBay`) REFERENCES `sanbay` (`soHieu`);
+ALTER TABLE ghe ADD FOREIGN KEY (soHieuMayBay) REFERENCES maybay (soHieuMayBay);
 
-ALTER TABLE `phieudatcho` ADD CONSTRAINT `maVe` FOREIGN KEY (`maVe`) REFERENCES `vemaybay` (`maVe`);
-#ALTER TABLE `phieudatcho` ADD CONSTRAINT `maKH` FOREIGN KEY (`maKH`) REFERENCES `khachhang` (`maKH`); #Lỗi dòng này ahuhu
+ALTER TABLE phieudatcho ADD FOREIGN KEY (maVe) REFERENCES vemaybay (maVe);
+ALTER TABLE phieudatcho ADD FOREIGN KEY (maKH) REFERENCES khachhang (maKH);
+
 
 
